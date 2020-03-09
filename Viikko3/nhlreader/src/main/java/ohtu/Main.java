1dc9457484454181580f4153c1f0639ba06bb0e4
Annukka Närhi <annukka.narhi@gmail.com>
2020-03-09 10:33:58 +0200

    Viikko 3 tehtävä 2 valmis

---
+++ Viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import org.apache.http.client.fluent.Request;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+                
+        System.out.println("json-muotoinen data:");
+        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        Arrays.sort(players, (Player a, Player b) -> {
+            return b.getPoints() - a.getPoints();
+        });
+        System.out.println("Oliot:");
+        for (Player player : players) {
+            if(player.getNationality().equalsIgnoreCase("fin"))
+            System.out.println(player);
+        }   
+    }
+  
+}
