f9d6f392fbbac6576393c4a56f5916932e1597c6
Annukka Närhi <annukka.narhi@gmail.com>
2020-03-10 20:57:47 +0200

    Viikko3 tehtävä 8 valmis

---
+++ Viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -0,0 +1,115 @@
+package ohtu;
+
+import java.util.Random;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        
+     // Onnistunut kirjautuminen ja uloskirjautuminen   
+    WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:4567");
+
+        sleep(2);
+
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        Random r = new Random();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("arto" + r.nextInt(100000));
+
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("signup"));
+
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        
+        element=driver.findElement(By.linkText("continue to application mainpage"));
+
+        sleep(2);
+        element.click();
+        sleep(3);
+        driver.quit();
+    }
+
+
+    // Onnistunnut kirjautuminen
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.get("http://localhost:4567");
+//
+//        sleep(2);
+//
+//        WebElement element = driver.findElement(By.linkText("register new user"));
+//        element.click();
+//
+//        sleep(2);
+//
+//        Random r = new Random();
+//
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("arto" + r.nextInt(100000));
+//
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("passwordConfirmation"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("signup"));
+//
+//        sleep(2);
+//        element.submit();
+//
+//        sleep(3);
+//
+//        driver.quit();
+//    }
+//     
+
+
+// Epäonnistunut kirjautuminen
+//      WebDriver driver = new ChromeDriver();
+//
+//        driver.get("http://localhost:4567");
+//        
+//        sleep(2);
+//        
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        sleep(2);
+//
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akep");
+//        element = driver.findElement(By.name("login"));
+//        
+//        sleep(2);
+//        element.submit();
+//
+//        sleep(3);
+//        
+//        driver.quit();
+//    }
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
+    }
+}
