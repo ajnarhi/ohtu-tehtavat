bd77d672212a092f493bafbac452e694a460ead8
Annukka Närhi <annukka.narhi@gmail.com>
2020-03-16 11:23:26 +0200

    Tehtävä 4 valmis, kauppa testit 100%

--- Viikko4/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ Viikko4/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -91,15 +91,15 @@ public class KauppaTest {
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 8);
 
     }
-    
-        @Test
+
+    @Test
     public void kahdenSamanOstoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaanJaPalautetaanOikeatArvot() {
 
         when(viite.uusi()).thenReturn(42);
         when(varasto.saldo(1)).thenReturn(10);
-       
+
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 4));
-       
+
         // sitten testattava kauppa 
         Kauppa k = new Kauppa(varasto, pankki, viite);
 
@@ -113,17 +113,17 @@ public class KauppaTest {
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 8);
 
     }
-    
-           @Test
+
+    @Test
     public void kaksiEriOstostaToistaOntoistaEiOlePaaytyttyaPankinMetodiaTilisiirtoKutsutaanJaPalautetaanOikeatArvot() {
 
         when(viite.uusi()).thenReturn(42);
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.saldo(2)).thenReturn(0);
-       
+
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 4));
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 3));
-       
+
         // sitten testattava kauppa 
         Kauppa k = new Kauppa(varasto, pankki, viite);
 
@@ -137,5 +137,93 @@ public class KauppaTest {
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 4);
 
     }
+
+    @Test
+    public void aloitaAsiointiNollaaEdellisenOstoksenTiedot() {
+
+        when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 4));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 3));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);   //ostetaan tuotetta numero 2 eli kahvia jota ei ole
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla parametreilla
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 3);
+
+    }
+
+    @Test
+    public void kauppaPyytaaViitenumeronJokaiselleMaksutapahtumalle() {
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 4));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 3));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();k.aloitaAsiointi();
+        k.lisaaKoriin(2);   //ostetaan tuotetta numero 2 eli kahvia jota ei ole
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        // tarkistetaan että tässä vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kerran
+        verify(viite, times(1)).uusi();
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+
+        // tarkistetaan että tässä vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kaksi kertaa
+        verify(viite, times(2)).uusi();
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+
+        // tarkistetaan että tässä vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kolme kertaa        
+        verify(viite, times(3)).uusi();
+
+    }
+    
+    
+    @Test
+    public void poistaKoristaPoistaaKerranKorista() {
+        Tuote t= new Tuote (1, "maito", 4);
+        when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+
+        when(varasto.haeTuote(1)).thenReturn(t);
+       
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(varasto, times(1)).palautaVarastoon(t);
+
+    }
+
 }
+
 
