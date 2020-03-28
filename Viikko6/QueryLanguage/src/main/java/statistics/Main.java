69565d6a70c94a521f2fd3a2dfb581b33224a6ae
Annukka Närhi <annukka.narhi@gmail.com>
2020-03-28 09:38:03 +0200

    Viikko 6 tehtävä 3 valmis

--- Viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ Viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -11,10 +11,26 @@ public class Main {
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
         
-        Matcher m = new And( 
-    new HasFewerThan(1, "goals"), 
-    new PlaysIn("NYR")
-);
+        
+        
+        Matcher m = new And(
+    new HasAtLeast(20, "points"),
+    new Or( 
+        new PlaysIn("NYR"),
+        new PlaysIn("NYI"),
+        new PlaysIn("NJD")
+    )
+); 
+        
+//        Matcher m = new Or( new HasAtLeast(20, "goals"),
+//                    new HasAtLeast(20, "assists")
+//); 
+//        
+    
+//        Matcher m = new And( 
+//    new HasFewerThan(1, "goals"), 
+//    new PlaysIn("NYR")
+//);
 
 //        
 //        Matcher m = new And( 
---
+++ Viikko6/QueryLanguage/src/main/java/statistics/matcher/Or.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+import statistics.Player;
+
+/**
+ *
+ * @author ajnarhi
+ */
+public class Or implements Matcher {
+    
+    
+        
+        private Matcher[] matchers;
+
+    public Or(Matcher... matchers) {
+        this.matchers = matchers;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        boolean matchinthebeginning=false;
+        for (Matcher matcher : matchers) {
+            if (matcher.matches(p)) {
+                matchinthebeginning=true;
+            }
+        }
+
+        return matchinthebeginning;
+    }
+    }
+    
+
